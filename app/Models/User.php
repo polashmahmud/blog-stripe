9de8d150a0ab1976f582ea1f08403cb804f093b2
root <root@localhost.localdomain>
2022-05-27 22:18:12 +0600

    policy added

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Profile;
+use App\Traits\ModelHelpers;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -17,6 +20,16 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use ModelHelpers;
+
+    const DEFAULT = 1;
+    const MODERATOR = 2;
+    const WRITER = 3;
+    const ADMIN = 4;
+
+    const TABLE = 'users';
+
+    protected $table = self::TABLE;
 
     /**
      * The attributes that are mass assignable.
@@ -27,6 +40,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type'
     ];
 
     /**
@@ -58,4 +72,44 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function emailAddress(): string
+    {
+        return $this->email;
+    }
+
+    public function type(): int
+    {
+        return (int) $this->type;
+    }
+
+    public function isModerator(): bool
+    {
+        return $this->type() === self::MODERATOR;
+    }
+
+    public function isWriter(): bool
+    {
+        return $this->type() === self::WRITER;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->type() === self::ADMIN;
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
